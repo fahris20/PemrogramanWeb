0f86e6981974529f5c71178b6a250a2264aab9fc
fahris20 <fhagaming3@gmail.com>
2022-09-23 10:36:34 +0700

    fahris

--- Pertemuan1/MembuatWebsite/footer.php
+++
@@ -1,6 +0,0 @@
-  <!-- footer -->
-  <div class="container rounded-bottom text-bg-dark  text-center py-3 mb-5" id="Footer">
-            <div class="">
-              <p class="">&copy; FIGURE FANTASY || DIBUAT OLEH SI PEMBUAT TERIMA KASIH</p>
-            </div>
-    </div>
---
+++ Pertemuan1/MembuatWebsite/navigasi.php
@@ -0,0 +1,33 @@
+<!-- navbar -->
+<div class="container mt-3 rounded-top" style="background-color: rgba(224,224,224,0.5);">
+        <nav class="navbar navbar-expand-lg text-light">
+            <div class="container-fluid">
+                <!-- Logo -->
+              <a class="navbar-brand" href="#">
+                <h1>Pemrograman</h1>
+              </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <!-- search -->
+                <form class="d-flex px-5" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success px-5" type="submit">Search</button>
+                </form>
+                <!-- Menu -->
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#Home">HOME</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#Product">PRODUCT</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#Contact">CONTACT US</a>
+                  </li> 
+                </ul>
+              </div>
+            </div>
+        </nav>
+    </div>
